da8787fb8156a2ec826c613f6eb685fb386b4d67
Alumno <ulsa@D104.local>
2020-11-10 15:01:58 -0700

    ListaEventos1/2

---
+++ listaEventos/CeldaEventoController.swift
@@ -0,0 +1,19 @@
+//
+//  CeldaEventoController.swift
+//  listaEventos
+//
+//  Created by Alumno on 11/10/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaEventoController : UITableViewCell{
+    @IBOutlet weak var lblTitulo: UILabel!
+    @IBOutlet weak var lblLugar: UILabel!
+    @IBOutlet weak var lblFecha: UILabel!
+    @IBOutlet weak var lblHora: UILabel!
+    @IBOutlet weak var imgEvento: UIImageView!
+    
+}
---
+++ listaEventos/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  listaEventos
+//
+//  Created by Alumno on 11/10/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var eventos : [Evento] = []
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celdaEvento =  tableView.dequeueReusableCell(withIdentifier: "celdaEvento") as! CeldaEventoController
+        
+        return celdaEvento
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
